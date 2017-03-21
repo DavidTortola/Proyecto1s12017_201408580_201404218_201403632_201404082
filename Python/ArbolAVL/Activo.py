c2255ee9d9a9f6cac7b81080b93d1c3f72859fc2
Andree Avalos <201408580@ingenieria.usac.edu.gt>
2017-03-21 17:23:36 -0600

    Clase Activo

---
+++ Python/ArbolAVL/Activo.py
@@ -0,0 +1,21 @@
+class Activo(object):
+	def __init__(self):
+		self.identificador=None
+		self.nombre=None
+		self.descripcion=None
+
+	def setId(self,identificador):
+		self.identificador=identificador
+	def setNombre(self,nombre):
+		self.nombre=nombre
+	def setDescripcion(self,descripcion):
+		self.descripcion=descripcion
+		
+
+	def getId(self):
+		return self.identificador
+	def getNombre(self):
+		return self.nombre
+	def getDescripcion(self):
+		return self.descripcion
+
